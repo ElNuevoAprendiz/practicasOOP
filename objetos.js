9957bf50c8c58f2811f2b12dad4ea7456578c8e8
Dan <profedanieljuan@gmail.com>
2024-06-26 11:40:38 -0300

    Se empieza a practicar objetos junto conla explicacion de que es un json el 7/5 en 0.13.13. Se agrega a la carpeta el peliculas.json, ejemplo que da el profe

--- objetos.js
+++ objetos.js
@@ -4,3 +4,11 @@ let auto1{
     patente:'DXC222',
     color:blanco,
 }
+
+let persona = {
+    nombre: "Juan", //variable del objeto. Par variable: valor,
+    apellido: "Paz",
+    dni: 11223344,
+}
+console.log(persona) // Imprimo el objeto
+console.log(persona.nombre) // Imprimo una propiedad del objeto: Juan
